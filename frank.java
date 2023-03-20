51469ac7043a82d71ae1e00d7442eb9207a6b3ac
frankpeterson <127899104+frankpeterson@users.noreply.github.com>
2023-03-20 03:37:14 +0000

    pi

--- frank.java
+++ frank.java
@@ -1,21 +1,36 @@
-public class frank{
+public class frank {
 
-    public static int power(int a, int b){
+    public static int power(int a, int b) {
         if (b == 1) {
             return a;
         } else {
-            int nextPower = power(a,b/2);
-            if(b % 2 == 0){
+            int nextPower = power(a, b / 2);
+            if (b % 2 == 0) {
                 return nextPower * nextPower;
+            } else {
+                return nextPower * nextPower * a;
             }
-            else {return nextPower*nextPower*a;
+        }
+    }
+
+
+    public static float pi(float p, int n, boolean subtract) {
+        if (n < 1000) {
+            if (subtract == true) {
+                p -= (4.0 / (n + 2));
+            } else {
+                p += (4.0 / (n + 2));
             }
+            n += 2;
+            subtract = !subtract;
+            return pi(p, n, subtract);
+        } else
+            return p;
     }
-    }
-        
-    
+
     public static void main(String[] args) {
-        System.out.println(power(4,8));
-        System.out.println(power(4,7));
+        // System.out.println(pi(0.0, 1, true));
+        System.out.println(pi(4, 1, true));
+
     }
 }
