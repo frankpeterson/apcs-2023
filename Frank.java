3564af7e69311c05c7cfbcdeaccbe680fde37624
frankpeterson <127899104+frankpeterson@users.noreply.github.com>
2023-03-21 02:57:46 +0000

    3/20 HW

--- Frank.java
+++ Frank.java
@@ -1,4 +1,6 @@
-public class frank {
+import java.util.Arrays;
+
+public class Frank {
 
     public static int power(int a, int b) {
         if (b == 1) {
@@ -27,10 +29,27 @@ public class frank {
         } else
             return p;
     }
+    
+//     To shuffle an array a of n elements (indices 0..n-1):
+// for i from n−1 downto 1 do
+//      j ← random integer such that 0 ≤ j ≤ i
+//      exchange a[j] and a[i]
+
+    public static String[] fishyate(String[] m){
+        for(int i = m.length-1; i >= 0; i--){
+            int random = (int)(Math.random()*i);
+            String temp = m[random];
+            m[random] = m[i];
+            m[i] = temp;
+        }
+        return (m);
+    }
 
     public static void main(String[] args) {
         // System.out.println(pi(0.0, 1, true));
-        System.out.println(pi(4, 1, true));
+        // System.out.println(pi(4, 1, true));
+        String[] aoeu = new String[]{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p"};
+        System.out.println(Arrays.toString(fishyate(aoeu)));
 
     }
 }
